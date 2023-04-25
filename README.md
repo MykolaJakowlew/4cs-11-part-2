75362cd2f7688dd40c3ac9ade0126c5a5df6e885
chatbotbuilder <robotbuilder@chatbots.studio>
2023-04-25 09:35:39 +0300

    save

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 1. npm install --save-dev jest
 2. npm install -D @types/jest
 3. ./node_modules/jest/bin/jest.js --init
+
+## Setup integration tests
+
+1. npm i mongodb-memory-server
--- src/api/handlers/dishes/create.spec.js
+++ src/api/handlers/dishes/create.spec.js
@@ -3,57 +3,57 @@ const Models = require('../../../models');
 
 const DishesMock = {};
 jest.mock('../../../models', () => {
- DishesMock.save = jest.fn();
- return {
-  Dishes: jest.fn().mockImplementation(() => ({
-   save: DishesMock.save
-  }))
- };
+  DishesMock.save = jest.fn();
+  return {
+    Dishes: jest.fn().mockImplementation(() => ({
+      save: DishesMock.save
+    }))
+  };
 });
 
 describe("createDish", () => {
- const res = {
-  status: jest.fn().mockImplementation(() => res),
-  send: jest.fn(),
- };
+  const res = {
+    status: jest.fn().mockImplementation(() => res),
+    send: jest.fn(),
+  };
 
- describe("default value isAvailable", () => {
-  it("is use ", async () => {
-   const req = { body: { price: 23 } };
-   const spyOn = jest.spyOn(Models, 'Dishes');
-   await createDish(req, res);
-   expect(spyOn.mock.calls[0])
-    .toEqual([{ ...req.body, isAvailable: true }]);
+  describe("default value isAvailable", () => {
+    it("is use ", async () => {
+      const req = { body: { price: 23 } };
+      const spyOn = jest.spyOn(Models, 'Dishes');
+      await createDish(req, res);
+      expect(spyOn.mock.calls[0])
+        .toEqual([{ ...req.body, isAvailable: true }]);
+    });
+
+    it("is not use ", async () => {
+      const req = { body: { price: 23, isAvailable: false } };
+      const spyOn = jest.spyOn(Models, 'Dishes');
+      await createDish(req, res);
+      expect(spyOn.mock.calls[0])
+        .toEqual([req.body]);
+    });
   });
 
-  it("is not use ", async () => {
-   const req = { body: { price: 23, isAvailable: false } };
-   const spyOn = jest.spyOn(Models, 'Dishes');
-   await createDish(req, res);
-   expect(spyOn.mock.calls[0])
-    .toEqual([req.body]);
-  });
- });
-
- it('data will be save in collection', async () => {
-  const req = { body: { price: 23 } };
-  await createDish(req, res);
-  expect(DishesMock.save).toBeCalled();
- });
-
- describe('response will be correct', () => {
-  it("should return 200 status code", async () => {
-   const req = { body: { price: 23 } };
-   await createDish(req, res);
-   expect(res.status).toBeCalledWith(200);
+  it('data will be save in collection', async () => {
+    const req = { body: { price: 23 } };
+    await createDish(req, res);
+    expect(DishesMock.save).toBeCalled();
   });
 
-  it("should return doc from collection", async () => {
-   const req = { body: { price: 23 } };
-   await createDish(req, res);
-   expect(res.status).toBeCalledWith(200);
-  });
+  describe('response will be correct', () => {
+    it("should return 200 status code", async () => {
+      const req = { body: { price: 23 } };
+      await createDish(req, res);
+      expect(res.status).toBeCalledWith(200);
+    });
 
- });
+    it("should return doc from collection", async () => {
+      const req = { body: { price: 23 } };
+      await createDish(req, res);
+      expect(res.status).toBeCalledWith(200);
+    });
+
+  });
 
 });
